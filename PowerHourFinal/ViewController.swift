f79f12d74b728ba0687dca518bc493ebd3676ca8
enstulen <enstulen@Mortens-MBP.lan>
2015-01-23 12:28:16 +0100

    Initial Commit

---
+++ PowerHourFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PowerHourFinal
+//
+//  Created by Morten Stulen on 23.01.15.
+//  Copyright (c) 2015 Morten Stulen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
